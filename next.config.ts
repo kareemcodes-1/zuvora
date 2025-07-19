6e50c28766c2a3214d6a14bcb0a8588e84a1eb2a
Kareem Braimoh <kareembrai222@gmail.com>
2025-07-19 05:15:30 +0100

    for test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // WARNING: disables build-time type checking — use at your own risk
+    ignoreBuildErrors: true,
+  },
+   images: {
     domains: ['framerusercontent.com', 'cdn.shopify.com', 'localhost', 'sklep099968.shoparena.pl', 'i.pinimg.com']
-  }
+  },
+  eslint: {
+    // also ignores lint errors during build
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
