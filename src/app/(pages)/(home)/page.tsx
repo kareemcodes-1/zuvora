cb0c3b3c8e75eae9f2537177615985a8b58445c7
Kareem <kareembrai222@gmail.com>
2026-03-21 13:30:33 +0100

    hi

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
 
   return (
-    <LenisProvider>
+    <>
 
       <Hero />
 
@@ -29,7 +29,7 @@ export default function Home() {
 
       <Testimonials />
 
-    </LenisProvider>
+    </>
   );
 }
 
