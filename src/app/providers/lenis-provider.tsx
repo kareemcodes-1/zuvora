c08d671cf808b82cb652fbdca7b5cfeafdd9007d
Kareem <kareembrai222@gmail.com>
2026-03-21 13:29:20 +0100

    hi

--- src/app/providers/lenis-provider.tsx
+++ src/app/providers/lenis-provider.tsx
@@ -1,24 +1,9 @@
-"use client";
-import React, { ReactNode, useEffect } from 'react'
-import Lenis from 'lenis'
-import 'lenis/dist/lenis.css'
-
-const LenisProvider = ({children}: {children: ReactNode}) => {
-
-      useEffect(() => {
-      // Initialize Lenis
-       const lenis = new Lenis({
-          autoRaf: true,
-       });
-
-      lenis.on("scroll", (e) => {
-        //  console.log(e);
-       });
-
-
-  }, [])
-
-  return children;
+function LenisProvider() {
+  return ( 
+    <div>
+      
+    </div>
+   );
 }
 
-export default LenisProvider
+export default LenisProvider;
